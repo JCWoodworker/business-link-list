28d8cd10521c11fe6d8bc1d59adebe10e012207e
JCWoodworker <jc@rilocalwoodworks.com>
2023-08-13 05:53:21 -0400

    added contact link, commented out free cutting board giveaway link

--- src/hardCodedLinks/hardCodedLinks.ts
+++ src/hardCodedLinks/hardCodedLinks.ts
@@ -5,10 +5,10 @@ import {
 } from "../types/interfaces"
 
 export const hardCodedLinks: HardCodedWebLinks[] = [
-	{
-		name: 'Win a free cutting board today!!\nField Of Artisans @ The General\'s Market',
-		url: "http://www.rilocalwoodworks.com/mailinglist",
-	},
+	// {
+	// 	name: 'Win a free cutting board today!!\nField Of Artisans @ The General\'s Market',
+	// 	url: "http://www.rilocalwoodworks.com/mailinglist",
+	// },
 	// {
 	// 	name: "Get 20% off your first online purchase!",
 	// 	url: "http://www.rilocalwoodworks.com/mailinglist",
@@ -20,6 +20,10 @@ export const hardCodedLinks: HardCodedWebLinks[] = [
 	{
 		name: "Portfolio",
 		url: "https://rilocalwoodworks.com/commissioned-pieces",
+	},
+	{
+		name: "Contact",
+		url: "https://rilocalwoodworks.com/contact",
 	},
 	{
 		name: "Purchase a gift card",
